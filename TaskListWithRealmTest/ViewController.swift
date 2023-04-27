433ee9a681b843a3caf545367a150c24626a9b32
Bellaaspera <lanasenatorova@gmail.com>
2023-04-27 18:02:31 +0300

    Initial Commit

---
+++ TaskListWithRealmTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskListWithRealmTest
+//
+//  Created by Светлана Сенаторова on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
